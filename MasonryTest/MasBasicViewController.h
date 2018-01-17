9f3c012511bcd20ca5060608640e617451636b85
YWSawyer <yuan_wei_1@163.com>
2018-01-17 14:19:19 +0800

    no message

---
+++ MasonryTest/MasBasicViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasBasicViewController.h
+//  MasonryTest
+//
+//  Created by dahuoshi on 15/01/2018.
+//  Copyright © 2018 dahuoshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasBasicViewController : UIViewController
+
+@end
---
+++ MasonryTest/MasBasicViewController.m
@@ -0,0 +1,37 @@
+//
+//  MasBasicViewController.m
+//  MasonryTest
+//
+//  Created by dahuoshi on 15/01/2018.
+//  Copyright © 2018 dahuoshi. All rights reserved.
+//
+
+#import "MasBasicViewController.h"
+
+@interface MasBasicViewController ()
+
+@end
+
+@implementation MasBasicViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
